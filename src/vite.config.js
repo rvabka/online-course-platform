75c0428210ca855661fbfd80209adc9ab01d78f4
Wiktor Stefaniak <wiktorstefaniak@macbook-pro-wiktor.home>
2025-11-12 21:25:46 +0100

    routes bug fix

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,16 +3,6 @@ import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    server: {
-        host: "0.0.0.0",
-        port: 5173,
-        hmr: {
-            host: "localhost",
-        },
-        watch: {
-            usePolling: true,
-        },
-    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
@@ -27,9 +17,4 @@ export default defineConfig({
             },
         }),
     ],
-    resolve: {
-        alias: {
-            "@": "/resources/js",
-        },
-    },
 });
